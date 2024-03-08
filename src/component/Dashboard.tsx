1ffa591420b84bcf154454b064d8146f05cb8fd2
trisouth <utku80@gmail.com>
2024-03-08 12:56:37 +0400

    test commit

--- src/component/Dashboard.tsx
+++ src/component/Dashboard.tsx
@@ -1,5 +1,22 @@
+/*
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div id="dashboard" className="container-fluid border border-dark">
+      <div
+        id="banner"
+        style={{ margin: "10px", border: "2px", borderColor: "orange" }}
+      >
+        utku
+      </div>
+    </div>
+  );
+};
+*/
+
 import React, { useContext } from "react";
-import { AuthContext } from "./context";
+import AuthContext from "../utils/context";
 
 const Dashboard = () => {
   const { sessionEnabled, username } = useContext(AuthContext);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //import "./index.css";
 import Login from "./component/Login.tsx";
-import Dashboard from "./component/Dashboard";
-import Test from "./component/Test"; // Import the Test component
+//import Dashboard from "./component/Dashboard";
+//import Test from "./component/Test"; // Import the Test component
 import "bootstrap/dist/css/bootstrap.css";
 
 /*
@@ -20,9 +20,6 @@ root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/test" element={<Test />} />{" "}
-      {/* Route for the Test component */}
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </Router>
 );
